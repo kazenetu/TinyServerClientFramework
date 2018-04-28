4cd53bc041825f3065c62ae615622ccb49a84ef6
kazenetu <rakudafactory@gmail.com>
2018-04-28 18:26:48 +0900

    Requestスーパークラスを追加

--- DataTransferObjects/Request/LoginRequest.cs
+++ DataTransferObjects/Request/LoginRequest.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataTransferObjects.Request
 {
   /// <summary>
   /// ログインリクエスト
   /// </summary>
-  public class LoginRequest
+  public class LoginRequest : RequestBase
   {
     /// <summary>
     /// ログインID
     /// </summary>
+    [Required]
     public string ID { set; get; }
 
     /// <summary>
     /// パスワード
     /// </summary>
+    [Required]
     public string Password { set; get; }
   }
 }
---
+++ DataTransferObjects/Request/RequestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Request
+{
+  /// <summary>
+  /// リクエストのスーパークラス
+  /// </summary>  
+  public class RequestBase
+  {
+    // 入力チェックNGとなったプロパティ名
+    public string ValidateNGPropertyName { get; protected set; } = string.Empty;
+
+    /// <summary>
+    /// 必須入力チェック
+    /// </summary>
+    /// <returns>結果</returns>
+    /// <remarks>任意の確認はサブクラスでオーバーライドする</remarks>
+    public virtual bool Validate()
+    {
+      // 必須プロパティのチェックを行う
+      var properties = this.GetType().GetProperties().Where(property => property.GetCustomAttributes(typeof(RequiredAttribute),false) != null);
+      foreach (var property in properties)
+      {
+        // nullの場合はチェックNG
+        if(property.GetValue(this) == null)
+        {
+          ValidateNGPropertyName = property.Name;
+          return false;
+        }
+      }
+
+      return false;
+    }
+  }
+}
