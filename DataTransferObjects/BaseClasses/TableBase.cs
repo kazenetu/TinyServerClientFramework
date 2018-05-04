6190f600d172bd3790c35d217be012bca0b41f08
kazenetu <rakudafactory@gmail.com>
2018-05-04 20:55:22 +0900

    テーブルDTOスーパークラスを追加

---
+++ DataTransferObjects/BaseClasses/TableBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace DataTransferObjects.BaseClasses
+{
+  /// <summary>
+  /// テーブルDTOのスーパークラス
+  /// </summary>
+  public abstract class TableBase
+  {
+    /// <summary>
+    /// DBカラム名とプロパティのコレクション取得
+    /// </summary>
+    /// <returns>DBカラム名とプロパティのコレクション</returns>
+    public abstract Dictionary<string, PropertyInfo> GetDBComlunProperyColection();
+  }
+}
