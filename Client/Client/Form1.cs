14ead0c647232c04e32938635db697926ee1cb7d
kazenetu <rakudafactory@gmail.com>
2018-05-05 20:59:54 +0900

    WebAPI発行処理をBusinessクラスの該当メソッドを呼ぶ方法に変更

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -1,4 +1,5 @@
-﻿using ClientFramework.ConnectLib;
+﻿using Client.Business;
+using ClientFramework.ConnectLib;
 using DataTransferObjects.Request;
 using DataTransferObjects.Response;
 using System;
@@ -14,67 +15,15 @@ namespace Client
       InitializeComponent();
     }
 
-    /// <summary>
-    /// WebAPIのルートURLを取得
-    /// </summary>
-    /// <param name="setApiPath">APIパスの追加</param>
-    /// <returns>APIのルートURL</returns>
-    private string getWebApiRootAddress(bool setApiPath)
-    {
-      var url = string.Empty;
-
-      if (ApplicationDeployment.IsNetworkDeployed)
-      {
-        // ClickOnce実行時のURL
-        var updateLocation = ApplicationDeployment.CurrentDeployment.UpdateLocation;
-        url = string.Format("{0}://{1}:{2}/", updateLocation.Scheme, updateLocation.Host, updateLocation.Port);
-      }
-      else
-      {
-        // デバッグ実行時のURL
-        var args = Environment.GetCommandLineArgs();
-        if (args.Length >= 2)
-        {
-          url = args[1];
-          if (url[url.Length - 1] != '/')
-          {
-            url += "/";
-          }
-        }
-        else
-        {
-          url = "http://localhost:5000/";
-        }
-      }
-
-      // APIパス追加
-      if (setApiPath)
-      {
-        url += "api/";
-      }
-
-      return url;
-    }
-
     private void webAPIGet_Click(object sender, EventArgs e)
     {
       try
       {
         var param = new LoginRequest() { ID = "test", Password = "test" };
-        var uri = webAPIUrl.Text;
 
-        HttpConnectLib.StubWebAPIDelegate stub = null;
-
-#if STUB
-        stub = (url, data) =>
-        {
-          var response = data as LoginResponse;
-          response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "ダミー" };
-          return response;
-        };
-#endif
-
-        var result = HttpConnectLib.Get<LoginResponse>(string.Format("{0}api/values?id={1}&password={2}", uri, param.ID, param.Password), stub);
+        // GET WebAPI発行
+        var business = new SampleBusiness();
+        var result = business.GetSample(param);
 
         MessageBox.Show($"結果:{result.Result.ToString()}{Environment.NewLine}" +
                     $"message:{result.ErrorMessage}{Environment.NewLine}" +
@@ -90,33 +39,17 @@ namespace Client
     {
       try
       {
-        // WebAPIのルートURLを取得
-        var uri = webAPIUrl.Text;
-
-        // GetTokenメソッド発行
-        HttpConnectLib.GetToken(string.Format("{0}", uri));
-
-        HttpConnectLib.StubWebAPIDelegate stub = null;
-
-#if STUB
-        stub = (url, data) =>
-        {
-          var response = data as LoginResponse;
-          response.ErrorMessage = "テストメッセージ";
-          response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "POSTダミー" };
-          return response;
-        };
-#endif
+        var param = new LoginRequest() { ID = "test", Password = "test" };
 
         // ログインWebAPI発行
-        var param = new LoginRequest() { ID = "test", Password = "test" };
-        var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}api/values/login", uri), param, stub);
+        var business = new SampleBusiness();
+        var result = business.PostSample(param);
 
         // 結果を表示
         var sb = new System.Text.StringBuilder();
-        sb.AppendLine($"result = {results.Result}");
-        sb.AppendLine($"message = {results.ErrorMessage}");
-        sb.AppendLine($"data = {results.ResponseData?.Name}");
+        sb.AppendLine($"result = {result.Result}");
+        sb.AppendLine($"message = {result.ErrorMessage}");
+        sb.AppendLine($"data = {result.ResponseData?.Name}");
         MessageBox.Show(sb.ToString());
       }
       catch (Exception ex)
