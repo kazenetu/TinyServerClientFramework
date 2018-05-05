5477ab89983972c91d14df4ca7f25bbda7c1a198
kazenetu <rakudafactory@gmail.com>
2018-05-05 21:00:43 +0900

    WebAPI周りのテキストボックスを削除

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -28,28 +28,9 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.webAPIUrl = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
       this.webAPIGet = new System.Windows.Forms.Button();
       this.webAPIPost = new System.Windows.Forms.Button();
       this.SuspendLayout();
-      // 
-      // webAPIUrl
-      // 
-      this.webAPIUrl.Location = new System.Drawing.Point(96, 29);
-      this.webAPIUrl.Name = "webAPIUrl";
-      this.webAPIUrl.Size = new System.Drawing.Size(206, 19);
-      this.webAPIUrl.TabIndex = 0;
-      this.webAPIUrl.Text = "http://localhost:5000/";
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(22, 29);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(59, 12);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "WebAPIUrl";
       // 
       // webAPIGet
       // 
@@ -78,19 +59,13 @@
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Controls.Add(this.webAPIPost);
       this.Controls.Add(this.webAPIGet);
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this.webAPIUrl);
       this.Name = "Form1";
       this.Text = "Form1";
       this.ResumeLayout(false);
-      this.PerformLayout();
 
     }
 
     #endregion
-
-    private System.Windows.Forms.TextBox webAPIUrl;
-    private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Button webAPIGet;
     private System.Windows.Forms.Button webAPIPost;
   }
