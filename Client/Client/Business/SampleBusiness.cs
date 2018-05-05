7c90ccc14a58957c0d6d0b4dae0cebefca90c0ff
kazenetu <rakudafactory@gmail.com>
2018-05-05 20:55:48 +0900

    Businessサンプルクラスを追加

---
+++ Client/Client/Business/SampleBusiness.cs
@@ -0,0 +1,53 @@
+﻿using ClientFramework.BaseClasses;
+using ClientFramework.ConnectLib;
+using DataTransferObjects.Request;
+using DataTransferObjects.Response;
+
+namespace Client.Business
+{
+  public class SampleBusiness : BussinessBase
+  {
+
+    /// <summary>
+    /// GETメソッドのサンプル
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public LoginResponse GetSample(LoginRequest request)
+    {
+      var webAPIUrl = $"values?id={request.ID}&password={request.Password}";
+
+      HttpConnectLib.StubWebAPIDelegate stub = null;
+#if STUB
+      stub = (url, data) =>
+      {
+        var response = data as LoginResponse;
+        response.ErrorMessage = "テストメッセージ";
+        response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "POSTダミー" };
+        return response;
+      };
+#endif
+
+      return get<LoginResponse>(webAPIUrl);
+    }
+
+    public LoginResponse PostSample(LoginRequest request)
+    {
+      var webAPIUrl = $"values/login";
+      var param = new LoginRequest() { ID = "test", Password = "test" };
+
+      HttpConnectLib.StubWebAPIDelegate stub = null;
+#if STUB
+      stub = (url, data) =>
+      {
+        var response = data as LoginResponse;
+        response.ErrorMessage = "テストメッセージ";
+        response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "POSTダミー" };
+        return response;
+      };
+#endif
+
+      return post<LoginResponse>(webAPIUrl, param, stub);
+    }
+  }
+}
