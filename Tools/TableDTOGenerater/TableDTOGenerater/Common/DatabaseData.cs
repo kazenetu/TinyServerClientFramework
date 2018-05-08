12ad9785d8fe8f8663548668782d8d92976abbc3
kazenetu <rakudafactory@gmail.com>
2018-05-08 23:30:21 +0900

    論理テーブル名と論理カラム名をDBから取得する機能を追加

--- Tools/TableDTOGenerater/TableDTOGenerater/Common/DatabaseData.cs
+++ Tools/TableDTOGenerater/TableDTOGenerater/Common/DatabaseData.cs
@@ -95,7 +95,7 @@ namespace TableDTOGenerater.Common
     /// </summary>
     /// <param name="dbName"></param>
     /// <param name="connectionString"></param>
-    public void SetConnectString(string dbName,string connectionString)
+    public void SetConnectString(string dbName, string connectionString)
     {
       if (dbInfos.ContainsKey(dbName))
       {
@@ -117,11 +117,16 @@ namespace TableDTOGenerater.Common
       return string.Empty;
     }
 
+    /// <summary>
+    /// テーブル・カラム情報を取得
+    /// </summary>
+    /// <param name="dbName">DB名</param>
+    /// <returns>取得結果</returns>
     public List<string> GetTables(string dbName)
     {
       var result = new List<string>();
 
-      if(db != null)
+      if (db != null)
       {
         db.Dispose();
       }
@@ -140,32 +145,39 @@ namespace TableDTOGenerater.Common
       {
         case nameof(DatabaseTypes.sqlserver):
           db = new SQLServerDB(config);
+
           sql = @"select name tname from sys.objects where type = 'U' and is_ms_shipped=0;";
           break;
         case nameof(DatabaseTypes.postgres):
           db = new PostgreSQLDB(config);
+
           sql = @"select relname tname from pg_stat_user_tables;";
           break;
         case nameof(DatabaseTypes.sqlite):
           db = new SQLiteDB(config);
+
           sql = @"select name tname from sqlite_master where type = 'table';";
           break;
       }
 
       // DBが未設定の場合は終了
-      if(db == null)
+      if (db == null)
       {
         return result;
       }
 
+      // テーブル名を取得
       var dbResult = db.Fill(sql);
-      foreach(DataRow row in dbResult.Rows)
+      foreach (DataRow row in dbResult.Rows)
       {
         var originalTableName = row["tname"].ToString();
         var tableName = snakeCase2CamelCase(originalTableName);
+        var logicalName = getTableLogicalName(originalTableName, dbName);
 
-        var tableData = new TableData() {TableName=tableName,TableOriginalName=originalTableName };
-        tableData.Columns = GetColumn(originalTableName, db);
+        var tableData = new TableData() { TableName = tableName, TableOriginalName = originalTableName, TableLogicalName = logicalName };
+
+        // カラム情報を取得
+        tableData.Columns = GetColumn(originalTableName, dbName);
 
         result.Add(tableData.ToString());
       }
@@ -174,22 +186,154 @@ namespace TableDTOGenerater.Common
     }
 
     /// <summary>
+    /// テーブル論理名を取得
+    /// </summary>
+    /// <param name="tableName">物理テーブル名</param>
+    /// <param name="dbName">DB名</param>
+    /// <returns>テーブル論理名</returns>
+    /// <remarks>未設定・SQLiteの場合は物理テーブル名</remarks>
+    private string getTableLogicalName(string tableName, string dbName)
+    {
+      var sql = new StringBuilder();
+
+      switch (dbName.ToLower())
+      {
+        case nameof(DatabaseTypes.sqlserver):
+          // テーブル名取得SQL
+          sql.AppendLine(@"SELECT");
+          sql.AppendLine(@"    ep.value AS 'table_comment'");
+          sql.AppendLine(@"FROM");
+          sql.AppendLine(@"    sys.tables t");
+          sql.AppendLine(@"INNER JOIN");
+          sql.AppendLine(@"  sys.extended_properties ep");
+          sql.AppendLine(@"  ON");
+          sql.AppendLine(@"    ep.major_id = t.object_id");
+          sql.AppendLine(@"    AND ep.minor_id = 0");
+          sql.AppendLine(@"WHERE");
+          sql.AppendLine($"  t.name = '{tableName}'");
+          sql.AppendLine(@"  and ep.name LIKE 'MS_Description';");
+          break;
+        case nameof(DatabaseTypes.postgres):
+          // テーブル名取得SQL
+          sql.AppendLine(@"select");
+          sql.AppendLine(@"  pd.description as table_comment");
+          sql.AppendLine(@"from");
+          sql.AppendLine(@"   pg_stat_user_tables psut");
+          sql.AppendLine(@"  ,pg_description pd");
+          sql.AppendLine(@"where");
+          sql.AppendLine($"  psut.relname='{tableName}'");
+          sql.AppendLine(@"  and psut.relid=pd.objoid");
+          sql.AppendLine(@"  and pd.objsubid=0;");
+          break;
+        case nameof(DatabaseTypes.sqlite):
+          return tableName;
+      }
+
+      var dbResult = db.Fill(sql.ToString());
+      foreach (DataRow row in dbResult.Rows)
+      {
+        var result = row["table_comment"];
+        if (result == DBNull.Value || string.IsNullOrEmpty(result.ToString()))
+        {
+          return tableName;
+        }
+        return result.ToString();
+      }
+
+      return tableName;
+    }
+
+    /// <summary>
     /// テーブル検索してカラム情報リストを取得する
     /// </summary>
-    /// <param name="tableName"></param>
-    /// <param name="db"></param>
+    /// <param name="tableName">物理テーブル名</param>
     /// <returns></returns>
-    public List<TableColumnData> GetColumn(string tableName,IDatabase db)
+    public List<TableColumnData> GetColumn(string tableName, string dbName)
     {
       var result = new List<TableColumnData>();
 
+      var colmunLogicalNames = getLogicalColmunName(tableName, dbName);
+
       var sql = $"select * from {tableName};";
       var dbResult = db.Fill(sql);
-      foreach(DataColumn column in dbResult.Columns)
+      foreach (DataColumn column in dbResult.Columns)
       {
         var columnName = snakeCase2CamelCase(column.ColumnName);
         var dataType = column.DataType;
-        result.Add(new TableColumnData() { ColumnName = columnName, ColumnOriginalName = column.ColumnName, ColumnType = dataType });
+        var columnLogicalName = columnName;
+        if (colmunLogicalNames.ContainsKey(column.ColumnName))
+        {
+          columnLogicalName = colmunLogicalNames[column.ColumnName];
+        }
+
+        result.Add(new TableColumnData() { ColumnName = columnName, ColumnOriginalName = column.ColumnName, ColumnType = dataType, ColumnLogicalName = columnLogicalName });
+      }
+
+      return result;
+    }
+
+    /// <summary>
+    /// 物理カラムと論理カラム名のコレクションを取得
+    /// </summary>
+    /// <param name="tableName">テーブル名</param>
+    /// <returns>物理カラムと論理カラム名のコレクション</returns>
+    private Dictionary<string, string> getLogicalColmunName(string tableName, string dbName)
+    {
+      var result = new Dictionary<string, string>();
+
+      var sql = new StringBuilder();
+      switch (dbName.ToLower())
+      {
+        case nameof(DatabaseTypes.sqlserver):
+          // カラム名取得SQL
+          sql.AppendLine(@"SELECT");
+          sql.AppendLine(@"  c.name AS 'column_name',");
+          sql.AppendLine(@"  ep.value AS 'column_comment'");
+          sql.AppendLine(@"FROM");
+          sql.AppendLine(@"  sys.tables t");
+          sql.AppendLine(@"INNER JOIN");
+          sql.AppendLine(@"  sys.columns c");
+          sql.AppendLine(@"  ON");
+          sql.AppendLine(@"    c.object_id = t.object_id");
+          sql.AppendLine(@"INNER JOIN");
+          sql.AppendLine(@"  sys.extended_properties ep");
+          sql.AppendLine(@"  ON");
+          sql.AppendLine(@"    ep.major_id = c.object_id");
+          sql.AppendLine(@"    AND ep.minor_id = c.column_id");
+          sql.AppendLine(@"WHERE");
+          sql.AppendLine($"  t.name = '{tableName}'");
+          sql.AppendLine(@"  and ep.name LIKE 'MS_Description';");
+          break;
+
+        case nameof(DatabaseTypes.postgres):
+          // カラム名取得SQL
+          sql.AppendLine(@"select");
+          sql.AppendLine(@"  pa.attname as column_name,");
+          sql.AppendLine(@"  pd.description as column_comment");
+          sql.AppendLine(@"from");
+          sql.AppendLine(@"  pg_stat_all_tables psat");
+          sql.AppendLine(@"  ,pg_description     pd");
+          sql.AppendLine(@"  ,pg_attribute       pa");
+          sql.AppendLine(@"where");
+          sql.AppendLine($"  psat.relname='{tableName}'");
+          sql.AppendLine(@"  and psat.relid = pd.objoid");
+          sql.AppendLine(@"  and pd.objsubid <> 0");
+          sql.AppendLine(@"  and pd.objoid=pa.attrelid");
+          sql.AppendLine(@"  and pd.objsubid=pa.attnum");
+          break;
+
+        case nameof(DatabaseTypes.sqlite):
+          return result;
+      }
+
+      var dbResult = db.Fill(sql.ToString());
+      foreach (DataRow row in dbResult.Rows)
+      {
+        var logicalName = row["column_comment"].ToString();
+        if (!string.IsNullOrEmpty(logicalName))
+        {
+          result.Add(row["column_name"].ToString(), logicalName);
+        }
       }
 
       return result;
