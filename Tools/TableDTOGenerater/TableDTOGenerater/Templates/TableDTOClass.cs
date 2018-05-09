6581d5a8f002074f190dec8a3558dad9135dfb5f
kazenetu <rakudafactory@gmail.com>
2018-05-09 21:11:43 +0900

    T4ファイルを追加

---
+++ Tools/TableDTOGenerater/TableDTOGenerater/Templates/TableDTOClass.cs
@@ -0,0 +1,12 @@
+﻿using static TableDTOGenerater.Common.DatabaseData;
+
+namespace TableDTOGenerater.Templates
+{
+  partial class TableDTO
+  {
+    /// <summary>
+    /// テーブル情報
+    /// </summary>
+    public TableData Table { set; get; }
+  }
+}
