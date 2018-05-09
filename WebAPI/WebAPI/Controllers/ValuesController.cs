eae086794e472655ed92bb5058caa9c7139af523
kazenetu <rakudafactory@gmail.com>
2018-05-09 19:57:37 +0900

    不要なメソッドを削除

--- WebAPI/WebAPI/Controllers/ValuesController.cs
+++ WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -118,30 +118,5 @@ namespace WebAPI.Controllers
 
       return Json(new UsersResponse(status, message, resultParam));
     }
-
-    // GET api/values/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-      return "value";
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody]string value)
-    {
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody]string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
   }
 }
