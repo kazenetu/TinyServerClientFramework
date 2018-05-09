606dd405a5a14cf9a6c11ca570204d8ba29d454e
kazenetu <rakudafactory@gmail.com>
2018-05-09 20:13:35 +0900

    不要なusingを削除

--- DataTransferObjects/Tables/MtUser.cs
+++ DataTransferObjects/Tables/MtUser.cs
@@ -1,7 +1,6 @@
 ﻿using DataTransferObjects.BaseClasses;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
 
 namespace DataTransferObjects.Tables
