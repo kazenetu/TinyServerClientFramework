79ed3677dc1a0b7308f17bae87d779611dae44bc
kazenetu <rakudafactory@gmail.com>
2018-05-12 20:25:28 +0900

    ユーザー追加リクエストクラスを追加

---
+++ DataTransferObjects/Request/AddUserRequest.cs
@@ -0,0 +1,25 @@
+﻿using DataTransferObjects.Request.BaseClasses;
+
+namespace DataTransferObjects.Request
+{
+  /// <summary>
+  /// ユーザー追加リクエスト
+  /// </summary>
+  public class AddUserRequest : RequestBase
+  {
+    /// <summary>
+    /// ユーザーID
+    /// </summary>
+    public string UserId { set; get; }
+
+    /// <summary>
+    /// ユーザー名
+    /// </summary>
+    public string UserName { set; get; }
+
+    /// <summary>
+    /// パスワード
+    /// </summary>
+    public string Password { set; get; }
+  }
+}
