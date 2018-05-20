49048df50484f51a986b1422c3bd329aeda4c1cb
kazenetu <rakudafactory@gmail.com>
2018-05-20 22:50:54 +0900

    文字列で表現するbool用の定数フィールドを追加

--- DataTransferObjects/BaseClasses/TableBase.cs
+++ DataTransferObjects/BaseClasses/TableBase.cs
@@ -8,6 +8,20 @@ namespace DataTransferObjects.BaseClasses
   /// </summary>
   public abstract class TableBase
   {
+    #region 定数フィールド
+
+    /// <summary>
+    /// DB文字列 True
+    /// </summary>
+    public const string StringTrue = "1";
+
+    /// <summary>
+    /// DB文字列 False
+    /// </summary>
+    public const string StringFalse = "0";
+
+    #endregion
+
     /// <summary>
     /// DBカラム名とプロパティのコレクション取得
     /// </summary>
