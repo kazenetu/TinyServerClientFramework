a7c8f440d94747efde9bdbfc239112d2aec87f60
kazenetu <rakudafactory@gmail.com>
2018-05-21 21:53:34 +0900

    メソッドコメント追加

--- Client/Client/Business/SampleBusiness.cs
+++ Client/Client/Business/SampleBusiness.cs
@@ -12,8 +12,8 @@ namespace Client.Business
     /// <summary>
     /// GETメソッドのサンプル
     /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
+    /// <param name="request">入力リクエスト</param>
+    /// <returns>ログイン結果</returns>
     public LoginResponse GetSample(LoginRequest request)
     {
       var webAPIUrl = $"{WebAPIVersion}/values?id={request.ID}&password={request.Password}";
@@ -32,6 +32,11 @@ namespace Client.Business
       return get<LoginResponse>(webAPIUrl, stub);
     }
 
+    /// <summary>
+    /// POSTメソッドのサンプル
+    /// </summary>
+    /// <param name="request">入力リクエスト</param>
+    /// <returns>ログイン結果</returns>
     public LoginResponse PostSample(LoginRequest request)
     {
       var webAPIUrl = $"{WebAPIVersion}/values/login";
