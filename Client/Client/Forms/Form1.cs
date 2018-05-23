38a27e918b88272749b04c5c836dedf48696bc6b
kazenetu <rakudafactory@gmail.com>
2018-05-24 07:26:25 +0900

    スーパークラスをFormBaseに変更

--- Client/Client/Forms/Form1.cs
+++ Client/Client/Forms/Form1.cs
@@ -1,11 +1,12 @@
-﻿using Client.Business.User;
+﻿using Client.BaseClasses;
+using Client.Business.User;
 using DataTransferObjects.Request.User;
 using System;
 using System.Windows.Forms;
 
 namespace Client
 {
-  public partial class Form1 : Form
+  public partial class Form1 : FormBase
   {
     public Form1()
     {
