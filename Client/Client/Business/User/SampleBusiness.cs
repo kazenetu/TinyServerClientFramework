92bae9fd03e87b39bdde3e6ded3a84ed83527a63
kazenetu <rakudafactory@gmail.com>
2018-05-23 20:27:35 +0900

    ツールでの自動生成前提として、ビジネスクラスをメソッド単位に分けるように変更

--- Client/Client/Business/User/SampleBusiness.cs
+++ Client/Client/Business/User/SampleBusiness.cs
@@ -9,55 +9,7 @@ namespace Client.Business.User
   /// <summary>
   /// サンプルロジック
   /// </summary>
-  public class SampleBusiness : BussinessBase
+  public partial class SampleBusiness : BussinessBase
   {
-
-    /// <summary>
-    /// GETメソッドのサンプル
-    /// </summary>
-    /// <param name="request">入力リクエスト</param>
-    /// <returns>ログイン結果</returns>
-    public LoginResponse GetSample(LoginRequest request)
-    {
-      var webAPIUrl = $"{WebAPIVersion}/values?id={request.ID}&password={request.Password}";
-
-      HttpConnectLib.StubWebAPIDelegate stub = null;
-#if STUB
-      // WebAPI回避　ダミーデータを返す
-      stub = (url, data) =>
-      {
-        var response = data as LoginResponse;
-        response.ErrorMessage = "テストメッセージ";
-        response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "GETダミー" };
-        return response;
-      };
-#endif
-
-      return get<LoginResponse>(webAPIUrl, stub);
-    }
-
-    /// <summary>
-    /// POSTメソッドのサンプル
-    /// </summary>
-    /// <param name="request">入力リクエスト</param>
-    /// <returns>ログイン結果</returns>
-    public LoginResponse PostSample(LoginRequest request)
-    {
-      var webAPIUrl = $"{WebAPIVersion}/values/login";
-
-      HttpConnectLib.StubWebAPIDelegate stub = null;
-#if STUB
-      // WebAPI回避　ダミーデータを返す
-      stub = (url, data) =>
-      {
-        var response = data as LoginResponse;
-        response.ErrorMessage = "テストメッセージ";
-        response.ResponseData = new LoginResponse.LoginResponseParam() { Name = "POSTダミー" };
-        return response;
-      };
-#endif
-
-      return post<LoginResponse>(webAPIUrl, request, stub);
-    }
   }
 }
