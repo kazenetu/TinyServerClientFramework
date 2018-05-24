dad5692991b289ae399fe07548eb459a8f5149ea
kazenetu <rakudafactory@gmail.com>
2018-05-24 23:04:13 +0900

    クライアントプロジェクトファイルに追加ファイルを追加する処理を実装

--- Tools/SourceGenerater/SourceGenerater/GeneraterEngine/GenerateClient.cs
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/GenerateClient.cs
@@ -21,22 +21,34 @@ namespace SourceGenerater.GeneraterEngine
     {
       var targetT4 = new List<IForm>() { new Form(), new FormDesigner(),new Business() };
 
-      foreach(var t4 in targetT4)
+      var itemGroups = new StringBuilder();
+      foreach (var t4 in targetT4)
       {
         t4.BaseName = baseName;
         if (!CreateFile($"{clientRootPath}\\{t4.CreateFileName}",t4.TransformText()))
         {
           return false;
         }
+        itemGroups.Append(t4.ProjectElement);
       }
+
+      AddCSProject(clientRootPath, itemGroups.ToString());
 
       return true;
     }
 
+    /// <summary>
+    /// ビジネスクラスのメソッドと専用DTOを追加
+    /// </summary>
+    /// <param name="clientRootPath">クライアントプロジェクトのルートパス</param>
+    /// <param name="baseName">ベース名</param>
+    /// <param name="methodName">メソッド名</param>
+    /// <returns>生成成否</returns>
     public bool AddBusinessMethod(string clientRootPath, string baseName, string methodName)
     {
       var targetT4 = new List<IMethod>() { new BusinessMethod(), new Request(), new Response() };
 
+      var itemGroups = new StringBuilder();
       foreach (var t4 in targetT4)
       {
         t4.BaseName = baseName;
@@ -45,7 +57,10 @@ namespace SourceGenerater.GeneraterEngine
         {
           return false;
         }
+        itemGroups.Append(t4.ProjectElement);
       }
+
+      AddCSProject(clientRootPath, itemGroups.ToString());
 
       return true;
     }
@@ -77,6 +92,41 @@ namespace SourceGenerater.GeneraterEngine
       }
 
       return true;
+    }
+
+    /// <summary>
+    /// ClientプロジェクトファイルにItemGroupを追加する
+    /// </summary>
+    /// <param name="clientRootPath">クライアントプロジェクトのルートパス</param>
+    /// <param name="targetItemGroup">追加するItemGroup情報</param>
+    private void AddCSProject(string clientRootPath, string targetItemGroup)
+    {
+      var projectFile = $"{clientRootPath}\\Client.csproj";
+      if (!File.Exists(projectFile))
+      {
+        return;
+      }
+
+      var projectValue = string.Empty;
+
+      using (var sr = new StreamReader(projectFile))
+      {
+        projectValue = sr.ReadToEnd();
+      }
+
+      var addItenGroup = new StringBuilder();
+      addItenGroup.AppendLine("  <ItemGroup>");
+      addItenGroup.AppendLine(targetItemGroup);
+      addItenGroup.AppendLine("  <ItemGroup>");
+      addItenGroup.Append("</Project>");
+
+      var lastTagStartPos = projectValue.LastIndexOf("</Project>");
+      projectValue = projectValue.Substring(0, lastTagStartPos)+ addItenGroup.ToString();
+
+      using (var sw = new StreamWriter(projectFile, false, new UTF8Encoding(true)))
+      {
+        sw.Write(projectValue);
+      }
     }
   }
 }
