0f47fba1a3a3a5b395bc776136584cab742d688a
kazenetu <rakudafactory@gmail.com>
2018-05-24 21:47:35 +0900

    テストボタンのテキスト修正

--- Tools/SourceGenerater/SourceGenerater/MainForm.Designer.cs
+++ Tools/SourceGenerater/SourceGenerater/MainForm.Designer.cs
@@ -34,32 +34,32 @@
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(435, 54);
+      this.button1.Location = new System.Drawing.Point(324, 54);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(64, 28);
+      this.button1.Size = new System.Drawing.Size(175, 28);
       this.button1.TabIndex = 0;
-      this.button1.Text = "button1";
+      this.button1.Text = "Form・Business作成テスト";
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // button2
       // 
-      this.button2.Location = new System.Drawing.Point(424, 137);
+      this.button2.Location = new System.Drawing.Point(324, 137);
       this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.Size = new System.Drawing.Size(175, 40);
       this.button2.TabIndex = 1;
-      this.button2.Text = "button2";
+      this.button2.Text = "Businessメソッド・専用DTO\r\n追加テスト";
       this.button2.UseVisualStyleBackColor = true;
       this.button2.Click += new System.EventHandler(this.button2_Click);
       // 
-      // Form1
+      // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(570, 364);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
-      this.Name = "Form1";
+      this.Name = "MainForm";
       this.Text = "Form1";
       this.ResumeLayout(false);
 
