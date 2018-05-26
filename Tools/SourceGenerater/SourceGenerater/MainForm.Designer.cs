b2d05b491110f3a3064389405449d22a78132cdc
kazenetu <rakudafactory@gmail.com>
2018-05-27 00:58:00 +0900

    画面ID入力用コントロール追加

--- Tools/SourceGenerater/SourceGenerater/MainForm.Designer.cs
+++ Tools/SourceGenerater/SourceGenerater/MainForm.Designer.cs
@@ -34,11 +34,14 @@
       this.RootFolder = new System.Windows.Forms.TextBox();
       this.label3 = new System.Windows.Forms.Label();
       this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.ScreenID = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.CreateFormBus = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(546, 53);
+      this.button1.Location = new System.Drawing.Point(537, 143);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(175, 28);
       this.button1.TabIndex = 0;
@@ -86,11 +89,40 @@
       // 
       this.folderBrowserDialog1.ShowNewFolderButton = false;
       // 
+      // ScreenID
+      // 
+      this.ScreenID.FormattingEnabled = true;
+      this.ScreenID.Location = new System.Drawing.Point(86, 53);
+      this.ScreenID.Name = "ScreenID";
+      this.ScreenID.Size = new System.Drawing.Size(454, 20);
+      this.ScreenID.TabIndex = 11;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 56);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(40, 12);
+      this.label1.TabIndex = 12;
+      this.label1.Text = "画面ID";
+      // 
+      // CreateFormBus
+      // 
+      this.CreateFormBus.Location = new System.Drawing.Point(569, 51);
+      this.CreateFormBus.Name = "CreateFormBus";
+      this.CreateFormBus.Size = new System.Drawing.Size(143, 23);
+      this.CreateFormBus.TabIndex = 13;
+      this.CreateFormBus.Text = "Form・Business作成";
+      this.CreateFormBus.UseVisualStyleBackColor = true;
+      // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(783, 455);
+      this.Controls.Add(this.CreateFormBus);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.ScreenID);
       this.Controls.Add(this.RefRootFolder);
       this.Controls.Add(this.RootFolder);
       this.Controls.Add(this.label3);
@@ -111,6 +143,9 @@
     private System.Windows.Forms.TextBox RootFolder;
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    private System.Windows.Forms.ComboBox ScreenID;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button CreateFormBus;
   }
 }
 
