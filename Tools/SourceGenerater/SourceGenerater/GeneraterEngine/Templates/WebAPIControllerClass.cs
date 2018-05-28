647e046ed549c1b44f22b282937e15a795ebb7e2
kazenetu <rakudafactory@gmail.com>
2018-05-28 22:13:36 +0900

    WebAPIController生成テンプレートを追加

---
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerClass.cs
@@ -0,0 +1,25 @@
+﻿using SourceGenerater.GeneraterEngine.Interfaces;
+
+namespace SourceGenerater.GeneraterEngine.Templates
+{
+  partial class WebAPIController : IForm
+  {
+    public string BaseName { set; get; }
+    public string CreateFileName
+    {
+      get
+      {
+        return $"..\\..\\WebAPI\\WebAPI\\Controllers\\{WebAPIVersion.ToUpper()}\\{BaseName}\\{BaseName}Controller.cs";
+      }
+    }
+    public string ProjectElement
+    {
+      get
+      {
+        return string.Empty;
+      }
+    }
+    public string WebAPIVersion { get; set; } = "v1";
+    public string BasePath { set; get; } = string.Empty;
+  }
+}
