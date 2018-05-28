be895de82a260083013e2f87619e1f941a42cc51
kazenetu <rakudafactory@gmail.com>
2018-05-28 22:24:38 +0900

    WebAPIControllerメソッド追加テンプレートを追加

---
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerMethodClass.cs
@@ -0,0 +1,25 @@
+﻿using SourceGenerater.GeneraterEngine.Interfaces;
+
+namespace SourceGenerater.GeneraterEngine.Templates
+{
+  partial class WebAPIControllerMethod : IMethod
+  {
+    public string BaseName { set; get; }
+    public string CreateFileName
+    {
+      get
+      {
+        return $"..\\..\\WebAPI\\WebAPI\\Controllers\\{WebAPIVersion.ToUpper()}\\{BaseName}\\{MethodName}.cs";
+      }
+    }
+    public string ProjectElement
+    {
+      get
+      {
+        return string.Empty;
+      }
+    }
+    public string MethodName { set; get; }
+    public string WebAPIVersion { get; set; } = "v1";
+  }
+}
