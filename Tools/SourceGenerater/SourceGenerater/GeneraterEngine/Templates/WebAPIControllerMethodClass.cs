2e3d46506d11571a3d406160bf1b48c8aecb60e3
kazenetu <rakudafactory@gmail.com>
2018-05-29 19:59:15 +0900

    virtual/override キーワードを付加する機能を追加

--- Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerMethodClass.cs
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerMethodClass.cs
@@ -23,5 +23,25 @@ namespace SourceGenerater.GeneraterEngine.Templates
     public string WebAPIVersion { get; set; } = "v1";
     public string BasePath { set; get; } = string.Empty;
 
+    public string GetMethodKeyword()
+    {
+      var reg = new System.Text.RegularExpressions.Regex(@"[^0-9]");
+      var nowVersion = reg.Replace(WebAPIVersion, string.Empty);
+      var beforeVersion = int.Parse(nowVersion) - 1;
+      while (beforeVersion > 0)
+      {
+        var targetFile = System.IO.Path.GetFullPath(System.IO.Path.Combine(BasePath, CreateFileName));
+        targetFile = targetFile.Replace(WebAPIVersion.ToUpper(), $"V{beforeVersion}");
+        if (System.IO.File.Exists(targetFile))
+        {
+          return "override";
+        }
+
+        beforeVersion -= 1;
+      }
+
+      return "virtual";
+    }
+
   }
 }
