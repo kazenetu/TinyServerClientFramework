0435189c6821cfcc2bb768f7b7b1c68334a97eb3
kazenetu <rakudafactory@gmail.com>
2018-05-29 18:52:11 +0900

    GetSuperClassNameをClassに移動

--- Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerClass.cs
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/WebAPIControllerClass.cs
@@ -21,5 +21,24 @@ namespace SourceGenerater.GeneraterEngine.Templates
     }
     public string WebAPIVersion { get; set; } = "v1";
     public string BasePath { set; get; } = string.Empty;
+
+    public string GetSuperClassName()
+    {
+      var result = "ControllerWithRepositoryBase";
+      var reg = new System.Text.RegularExpressions.Regex(@"[^0-9]");
+      var nowVersion = reg.Replace(WebAPIVersion, string.Empty);
+      var beforeVersion = int.Parse(nowVersion) - 1;
+      if (beforeVersion >= 1)
+      {
+        var targetFile = System.IO.Path.GetFullPath(System.IO.Path.Combine(BasePath, CreateFileName));
+        targetFile = targetFile.Replace(WebAPIVersion.ToUpper(), $"V{beforeVersion}");
+        if (System.IO.File.Exists(targetFile))
+        {
+          result = $"WebAPI.Controllers.V{beforeVersion}.{BaseName}Controller";
+        }
+      }
+      return result;
+    }
+
   }
 }
