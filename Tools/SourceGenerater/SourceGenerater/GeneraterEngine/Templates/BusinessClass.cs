41f1f1e52d27f995ce5ac4608fc668f92c4245ca
kazenetu <rakudafactory@gmail.com>
2018-05-30 19:51:39 +0900

    不要なusingを削除

--- Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/BusinessClass.cs
+++ Tools/SourceGenerater/SourceGenerater/GeneraterEngine/Templates/BusinessClass.cs
@@ -1,9 +1,4 @@
 ﻿using SourceGenerater.GeneraterEngine.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /// <summary>
 /// Businessテンプレート
