074469e1511ee3d7654c09ec6294994a87244242
kazenetu <rakudafactory@gmail.com>
2018-05-31 18:41:56 +0900

    ダミーテーブルDTOを追加

---
+++ DataTransferObjects/Tables/DummyTable.cs
@@ -0,0 +1,28 @@
+﻿using DataTransferObjects.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DataTransferObjects.Tables
+{
+  /// <summary>
+  /// ダミーテーブルDTO
+  /// </summary>
+  /// <remarks>
+  /// 実際に利用するテーブルDTOまたはカスタマイズDTOに置き換えてください。
+  /// </remarks>
+  [Obsolete("テーブルDTOまたはカスタマイズDTOに置き換えてください。")]
+  public class DummyTable : TableBase
+  {
+    /// <summary>
+    /// DBカラム名とプロパティのコレクション取得
+    /// </summary>
+    /// <returns>DBカラム名とプロパティのコレクション</returns>
+    public override Dictionary<string, PropertyInfo> GetDBComlunProperyColection()
+    {
+      var result = new Dictionary<string, PropertyInfo>();
+
+      return result;
+    }
+  }
+}
