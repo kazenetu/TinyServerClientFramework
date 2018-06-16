9db72f4b8480f706b2cd5f5dd24c756a29bb2abf
kazenetu <rakudafactory@gmail.com>
2018-06-16 16:42:24 +0900

    注文一覧の検索結果用カスタムテーブルDTOを追加

---
+++ Example/DataTransferObjects/CustomTables/CustomTOrder.cs
@@ -0,0 +1,15 @@
+﻿using DataTransferObjects.Tables;
+
+namespace DataTransferObjects.CustomTables
+{
+  /// <summary>
+  /// 注文一覧用カスタムテーブルDTO
+  /// </summary>
+  public class CustomTOrder : TOrder
+  {
+    /// <summary>
+    /// 注文者名
+    /// </summary>
+    public string OrderUserName { set; get; }
+  }
+}
