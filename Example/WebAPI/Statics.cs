81a1a7956d07224acd50fa69da306a2e17711fd0
kazenetu <rakudafactory@gmail.com>
2018-06-21 20:35:29 +0900

    Stiatics.cs追加

---
+++ Example/WebAPI/Statics.cs
@@ -0,0 +1,23 @@
+﻿namespace Client
+{
+  /// <summary>
+  /// 定数クラス
+  /// </summary>
+  public static class Statics
+  {
+    /// <summary>
+    /// WebAPIのバージョン
+    /// </summary>
+    public static string WebAPIVersion = "v1";
+
+    /// <summary>
+    /// WebAPI結果Result:OK
+    /// </summary>
+    public const string ResultOK = "OK";
+
+    /// <summary>
+    /// WebAPI結果Result:NG
+    /// </summary>
+    public const string ResultNG = "NG";
+  }
+}
