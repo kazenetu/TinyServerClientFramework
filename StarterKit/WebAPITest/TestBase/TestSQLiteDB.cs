860e2d12d8207f504b98cdbf88b9cdb49e265dd7
kazenetu <rakudafactory@gmail.com>
2018-06-24 23:25:47 +0900

    テストプロジェクトのテスト用SQLiteDBにパラメータ設定メソッドのオーバーライドを追加

--- StarterKit/WebAPITest/TestBase/TestSQLiteDB.cs
+++ StarterKit/WebAPITest/TestBase/TestSQLiteDB.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
 using Framework.WebAPI.DB;
+using System;
 
 namespace WebAPITest.TestBase
 {
@@ -15,6 +16,31 @@ namespace WebAPITest.TestBase
     /// <param name="connectionString">接続文字列のコア部分</param>
     public TestSQLiteDB(string connectionString) : base(connectionString)
     {
+    }
+
+    /// <summary>
+    /// パラメータを追加
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <param name="value">値</param>
+    public override void AddParam(string key, object value)
+    {
+      switch (value)
+      {
+        case DateTime dt:
+          this.param.Add(key, dt.ToString("yyyy-MM-dd HH:mm:ss"));
+          break;
+        case object obj:
+          this.param.Add(key, obj);
+          break;
+        default:
+          if (value == null)
+          {
+            value = string.Empty;
+          }
+          this.param.Add(key, value);
+          break;
+      }
     }
 
     /// <summary>
