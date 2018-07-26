99de2651bdcac34c632135cb83befc70be233bfb
kazenetu <rakudafactory@gmail.com>
2018-07-26 20:04:52 +0900

    不要なusingを削除

--- Example/WebAPI/Program.cs
+++ Example/WebAPI/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 
 namespace WebAPI
 {
-    public class Program
+  public class Program
     {
         public static void Main(string[] args)
         {
