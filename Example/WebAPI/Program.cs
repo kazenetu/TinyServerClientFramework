98936cb87c76fdea2e429cda98325c0643203eac
kazenetu <rakudafactory@gmail.com>
2018-07-26 20:04:17 +0900

    NLogを使ってログ管理するよう機能追加

--- Example/WebAPI/Program.cs
+++ Example/WebAPI/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace WebAPI
 {
@@ -20,6 +21,11 @@ namespace WebAPI
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                  logging.ClearProviders();
+                })
+                .UseNLog()
                 .Build();
     }
 }
